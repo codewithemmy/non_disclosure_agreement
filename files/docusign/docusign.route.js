bbf37211c0b2aed563fbb2e73841b2a10c661ca6
codewithemmy <codewithemmy@gmail.com>
2023-12-17 09:44:51 +0100

    version

--- files/docusign/docusign.route.js
+++ files/docusign/docusign.route.js
@@ -9,10 +9,8 @@ const { isAuthenticated } = require("../../utils")
 const { contentParser } = require("../../utils/contentParser")
 
 docusignRouter
-  .route(
-    "/restapi/v2.1/accounts/6a609554-d9d7-43d0-a0a6-24638588457b/connect"
-  )
-  .post(contentParser, docusignWebhookController)
+  .route("/restapi/v2.1/accounts/6a609554-d9d7-43d0-a0a6-24638588457b/connect")
+  .post(docusignWebhookController)
 
 //authenticated routes go below here
 https: docusignRouter.use(isAuthenticated)
